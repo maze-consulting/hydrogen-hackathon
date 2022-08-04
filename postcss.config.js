20bb0ed25784669f67f30598a9e19c559815111d
David Moore <david@themazegroup.com>
2022-08-04 15:20:24 -0400

    Postcss Updated

--- postcss.config.js
+++ postcss.config.js
@@ -3,8 +3,27 @@ module.exports = {
     'postcss-import': {},
     'tailwindcss/nesting': {},
     tailwindcss: {},
-    'postcss-preset-env': {
-      features: {'nesting-rules': false},
+    'postcss-flexbugs-fixes': {},
+    'postcss-will-change-transition': {},
+    'postcss-focus-visible': {},
+    'postcss-pxtorem': {
+      rootValue: 16,
+      propList: ['*', '!border', '!border-width'],
     },
+    'postcss-preset-env': {
+      stage: 3,
+      features: {'nesting-rules': false},
+      autoprefixer: {
+        cascade: false,
+        flexbox: 'no-2009',
+      },
+    },
+    'postcss-discard-comments': {
+      removeAll: true,
+    },
+    'postcss-combine-duplicated-selectors': {
+      removeDuplicatedValues: true,
+    },
+    autoprefixer: {},
   },
 };
